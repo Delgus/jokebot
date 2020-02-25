95fcb42c2ede79224240c3467cc5baf49056b512
delgus <alexdolgovak47@mail.ru>
2020-02-25 23:07:00 +0300

    tg keyboard

--- internal/inrastructure/notify/tg.go
+++ internal/inrastructure/notify/tg.go
@@ -22,11 +22,11 @@ func NewTGNotifier(accessToken string, logger Logger) (*TGNotifier, error) {
 
 func (tgn *TGNotifier) SendMessage(userID int, text string) {
 	msg := tg.NewMessage(int64(userID), text)
-	msg.ReplyMarkup = tg.NewInlineKeyboardMarkup(
-		tg.NewInlineKeyboardRow(
-			tg.NewInlineKeyboardButtonData("Анекдот", "joke"),
-			tg.NewInlineKeyboardButtonData("Категории анекдотов", "list"),
-			tg.NewInlineKeyboardButtonData("Помощь", "help"),
+	msg.ReplyMarkup = tg.NewReplyKeyboard(
+		tg.NewKeyboardButtonRow(
+			tg.NewKeyboardButton("joke"),
+			tg.NewKeyboardButton("list"),
+			tg.NewKeyboardButton("help"),
 		),
 	)
 	_, err := tgn.client.Send(msg)
